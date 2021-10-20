ca7dbf6e31b38539bf8c82e84eb4d8e1ce10d8b7
vennapubhuvankumar <92288036+vennapubhuvankumar@users.noreply.github.com>
2021-10-20 09:53:12 +0530

    Add files via upload

---
+++ sum of two numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+//variable declaration
+{
+	int number1,number2, sum;
+	//initialization
+	number1 = 4566;
+	number2 = 544654;
+	//calculation
+	sum=number1+number2;
+	//output
+	printf("sum of two numbers %d", sum);
+}
