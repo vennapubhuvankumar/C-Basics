0306581b7e37fc379348e2bfafa942537b849300
vennapubhuvankumar <92288036+vennapubhuvankumar@users.noreply.github.com>
2021-10-20 10:13:26 +0530

    Add files via upload

---
+++ use of float,char,,int.c
@@ -0,0 +1,17 @@
+// use of char,float,int
+#include<stdio.h>
+void main()
+//variable declaration
+{
+	int age;
+	float marks;
+	char initial;
+	//initialization
+	age = 456766;
+	marks= 5454.56;
+	initial = 'b';
+	//output
+	printf("%d\n", age);
+	printf("%f\n", marks);
+	printf("\"%c\"",initial);
+	}
