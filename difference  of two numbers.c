e3cd908a4495e08ec8ab76fff48fbac7c7fc1468
vennapubhuvankumar <92288036+vennapubhuvankumar@users.noreply.github.com>
2021-10-20 09:56:41 +0530

    Add files via upload

---
+++ difference  of two numbers.c
@@ -0,0 +1,14 @@
+//difference of two mumbers
+#include<stdio.h>
+void main()
+//variable declaration
+{
+	int number1,number2, difference;
+	//initialization
+	number1 = 456766;
+	number2 = 5454;
+	//calculation
+	difference=number1-number2;
+	//output
+	printf("diffference of two numbers %d", difference);
+}
