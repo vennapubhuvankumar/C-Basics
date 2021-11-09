9f99ae6fb3721b0bc09600f31b38d701fd4a6a70
vennapubhuvankumar <92288036+vennapubhuvankumar@users.noreply.github.com>
2021-11-09 10:49:09 +0530

    Add files via upload

---
+++ ggg.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+int a;
+	printf("enter a number");
+	scanf("%d", &a);
+	printf("%c",a+96);
+}
---
+++ less and greater than.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int a, b;	
+	a= 10;
+	b= 20;
+	printf("%d < %d --> %d\n",a,b,a<b);
+	printf("%d <= %d --> %d\n",a,b,a<=b);
+}
