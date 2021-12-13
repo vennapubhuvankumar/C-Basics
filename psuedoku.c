2e3c7d5cce8a6d77df532e11dd94949a9a9f80e5
vennapubhuvankumar <92288036+vennapubhuvankumar@users.noreply.github.com>
2021-12-13 10:54:49 +0530

    Add files via upload

---
+++ psuedoku.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+ 	int a,b,c;
+ 	printf("enter three  numbers");
+ 	scanf("%d%d%d",&a,&b,&c);
+ 	if((a>b)|| (c+a) < (b-c))
+ 	
+ 	{
+ 		b=(12+10)+a;
+ 	b=c+b;
+ 	printf("%d",a+b+c);
+ }
+ else
+ 
+ {
+ 	c=c+c;
+ 	printf("%d",a+b+c);
+ }
+}
